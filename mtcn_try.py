53b6c1fb2c8727ebfa7da8ab95520936e3131676
swapnil londhe <37972263+swapnillondhe24@users.noreply.github.com>
2020-03-07 00:55:32 +0530

    Add files via upload

---
+++ mtcn_try.py
@@ -0,0 +1,37 @@
+import cv2
+from mtcnn.mtcnn import MTCNN
+from imutils_webcamcapture import WebcamVideoStream
+import imutils
+
+
+detector = MTCNN()
+cap = WebcamVideoStream(src=0).start()
+while True: 
+    #Capture frame-by-frame
+    frame = cap.read()
+    
+    #Use MTCNN to detect faces
+    result = detector.detect_faces(frame)
+    if result != []:
+        for person in result:
+            bounding_box = person['box']
+            keypoints = person['keypoints']
+    
+            cv2.rectangle(frame,
+                          (bounding_box[0], bounding_box[1]),
+                          (bounding_box[0]+bounding_box[2], bounding_box[1] + bounding_box[3]),
+                          (0,155,255),
+                          2)
+    
+            cv2.circle(frame,(keypoints['left_eye']), 2, (0,155,255), 2)
+            cv2.circle(frame,(keypoints['right_eye']), 2, (0,155,255), 2)
+            cv2.circle(frame,(keypoints['nose']), 2, (0,155,255), 2)
+            cv2.circle(frame,(keypoints['mouth_left']), 2, (0,155,255), 2)
+            cv2.circle(frame,(keypoints['mouth_right']), 2, (0,155,255), 2)
+    #display resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) &0xFF == ord('q'):
+        break
+#When everything's done, release capture
+cap.release()
+cv2.destroyAllWindows()
